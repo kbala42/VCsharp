59a83aebc9edb073d2d4b398a4b3cc25004b720b
Kamil <kbala42ylv@gmail.com>
2023-04-26 07:15:41 +0300

    upt_2023.04.26_1

---
+++ HesapMakinesiBasit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesiBasit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tekrar:
+            Console.WriteLine("Hesap makinesi");
+            Console.Write("Birinci sayıyı giriniz: "); int sayiBir = int.Parse(Console.ReadLine());
+            Console.Write("Ikinci sayıyı giriniz: "); int sayiIki = int.Parse(Console.ReadLine());
+            Console.WriteLine("Toplamak için  + tuşuna");
+            Console.WriteLine("Çıkartmak için - tuşuna");
+            Console.WriteLine("Çarpma için    * tuşuna");
+            Console.WriteLine("Bölmek için    - tuşuna");
+            Console.WriteLine("basınız");
+            String secim = Console.ReadLine();
+            int sonuc;
+            switch(secim)
+            {
+                case "+":
+                    sonuc = sayiBir + sayiIki;
+                    Console.WriteLine(sayiBir + " + " + sayiIki+ " =" + sonuc );
+                    break;
+
+                case "-":
+                    sonuc = sayiBir - sayiIki;
+                    Console.WriteLine(sayiBir + " + " + sayiIki + " =" + sonuc);
+                    break;
+
+                case "*":
+                    sonuc = sayiBir * sayiIki;
+                    Console.WriteLine(sayiBir + " + " + sayiIki + " =" + sonuc);
+                    break;
+
+                case "/":
+                    sonuc = sayiBir / sayiIki;
+                    Console.WriteLine(sayiBir + " + " + sayiIki + " =" + sonuc);
+                    break;
+
+            }
+            goto tekrar;
+        }
+    }
+}
